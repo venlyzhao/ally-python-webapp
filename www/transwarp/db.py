c7cff1a14223c375bc1fec3675f4662115d83a12
venlyzhao <venlyzhao@163.com>
2014-11-25 00:05:29 +0800

    update db.py

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -10,3 +10,15 @@ Database operation module.
 import time, uuid, functools, threading, logging 
  
  
+@with_connection
+def select_init(sql, *args):
+	...
+	Execute select SQL .
+
+	>>> n = update('delete from user')
+	>>> u1 = dict(id=96900, name='Ada', email='ada@test.org', passwd='A-12345', last_modified=time.time())
+    >>> u2 = dict(id=96901, name='Adam', email='adam@test.org', passwd='A-12345', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    
+	pass
